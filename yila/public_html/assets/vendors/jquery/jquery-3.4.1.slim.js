91044ecd8cfa38c5803b2bc418289ad6c514e4c6
Wyeben <yila.ben@yahoo.com>
2024-04-15 09:59:37 +0100

    Added changes

--- yila/public_html/assets/vendors/jquery/jquery-3.4.1.slim.js
+++ yila/public_html/assets/vendors/jquery/jquery-3.4.1.slim.js
@@ -1,16 +1,4 @@
-/*!
- * jQuery JavaScript Library v3.4.1 -ajax,-ajax/jsonp,-ajax/load,-ajax/parseXML,-ajax/script,-ajax/var/location,-ajax/var/nonce,-ajax/var/rquery,-ajax/xhr,-manipulation/_evalUrl,-event/ajax,-effects,-effects/Tween,-effects/animatedSelector
- * https://jquery.com/
- *
- * Includes Sizzle.js
- * https://sizzlejs.com/
- *
- * Copyright JS Foundation and other contributors
- * Released under the MIT license
- * https://jquery.org/license
- *
- * Date: 2019-05-01T21:04Z
- */
+
 ( function( global, factory ) {
 
 	"use strict";
@@ -510,16 +498,7 @@ function isArrayLike( obj ) {
 		typeof length === "number" && length > 0 && ( length - 1 ) in obj;
 }
 var Sizzle =
-/*!
- * Sizzle CSS Selector Engine v2.3.4
- * https://sizzlejs.com/
- *
- * Copyright JS Foundation and other contributors
- * Released under the MIT license
- * https://js.foundation/
- *
- * Date: 2019-04-08
- */
+
 (function( window ) {
 
 var i,
